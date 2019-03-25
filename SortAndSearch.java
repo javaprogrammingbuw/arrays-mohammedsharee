c0adaf15f6108b5b65d6a9818451f5c23ddea632
mohammedsharee <44450467+mohammedsharee@users.noreply.github.com>
2019-03-25 08:52:44 +0100

    Update SortAndSearch.java

--- SortAndSearch.java
+++ SortAndSearch.java
@@ -7,4 +7,56 @@ public class SortAndSearch {
 	//First, it sorts this array by using the bubblSrt method. Then, it searches for n.
 	//You can look up the binary search algorithm here: https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
 
-}
+	public static void main(String[] args) {
+	      int a[] = { 9, 2, -6, -9, 4, 12, 15 };
+	      System.out.println("Before  Sorting");
+
+	      for(int i = 0; i < a.length; i++) {
+	         System.out.print(a[i] + " ");
+	      }
+	      System.out.println();
+	      bubbleSort(a);
+	      System.out.println("After  Sorting");
+
+	      for(int i = 0; i < a.length; i++) {
+	         System.out.print(a[i] + " ");
+	      }
+	   }
+	
+	
+		public  static void bubbleSort(int[] a) {
+		      int n = a.length;
+		      int temp = 0;
+
+		      for(int i = 0; i < n; i++) {
+		         for(int j=1; j < (n-i); j++) {
+		            if(a[j-1] > a[j]) {
+		               temp = a[j-1];
+		               a[j-1] = a[j];
+		               a[j] = temp;
+		            }
+		         }
+		      }
+		   }
+		
+           public static int binSrch(int arr[], int a, int b, int c){ 
+	    
+	        if (b>=a) 
+	        { 
+	            int middle = a + (b - a)/2; 
+	   
+	      
+	            if (arr[middle] == c) 
+	               return middle; 
+	   
+	         
+	            if (arr[middle] > c) 
+	               return binSrch(arr, a, middle-1, c); 
+	   
+	            return binSrch(arr, middle+1, b, c); 
+	        } 
+	  
+	        return -1; 
+	    }
+	
+		}
